7d4f56c7652bd3fa20ff555d5ec5ffec0cd4b082
Luís Canadas <filipe.canadas@gmail.com>
2025-05-14 10:41:57 +0100

    Update columns options

--- resources/views/tables/columns/obfuscated-column.blade.php
+++ resources/views/tables/columns/obfuscated-column.blade.php
@@ -1,22 +1,39 @@
 @php
     $state = $getState();
     $obfuscated = $column->getObfuscatedValue($state);
+    $revealOnClick = $column->getRevealOnClick();
+    $revealOnHover = $column->getRevealOnHover();
 @endphp
 
-<div x-data="{ revealed: false }" @click="revealed = !revealed" class="flex items-center gap-2 cursor-pointer group">
+<div
+    @if ($revealOnClick) x-data="{ revealed: false }"
+        @click="revealed = !revealed"
+        class="flex items-center gap-2 cursor-pointer group"
+    @elseif($revealOnHover)
+        x-data="{ revealed: false }"
+        @mouseenter="revealed = true"
+        @mouseleave="revealed = false"
+        class="flex items-center gap-2 cursor-help group"
+    @else
+        class="flex items-center gap-2" @endif>
     <span class="text-sm" x-show="!revealed">{{ $obfuscated }}</span>
-    <span class="text-sm" x-show="revealed" x-cloak>{{ $state }}</span>
-    @if ($state !== null && $state !== '')
-        <svg class="w-4 h-4 text-gray-400 group-hover:text-primary-500" x-bind:class="{ 'text-primary-500': revealed }"
-            fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-            <path x-show="!revealed" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"></path>
-            <path x-show="!revealed" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z">
-            </path>
-            <path x-show="revealed" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                d="M13.875 18.825A10.05 10.05 0 0112 19c-4.478 0-8.268-2.943-9.543-7a9.97 9.97 0 011.563-3.029m5.858.908a3 3 0 114.243 4.243M9.878 9.878l4.242 4.242M9.88 9.88l-3.29-3.29m7.532 7.532l3.29 3.29M3 3l3.59 3.59m0 0A9.953 9.953 0 0112 5c4.478 0 8.268 2.943 9.543 7a10.025 10.025 0 01-4.132 5.411m0 0L21 21\">
-            </path>
-        </svg>
+    @if ($revealOnClick || $revealOnHover)
+        <span class="text-sm" x-show="revealed" x-cloak>{{ $state }}</span>
+        @if ($state !== null && $state !== '')
+            <svg class="w-4 h-4 text-gray-400 group-hover:text-primary-500"
+                x-bind:class="{ 'text-primary-500': revealed }" fill="none" stroke="currentColor" viewBox="0 0 24 24"
+                xmlns="http://www.w3.org/2000/svg">
+                <path x-show="!revealed" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                    d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"></path>
+                <path x-show="!revealed" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                    d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z">
+                </path>
+                <path x-show="revealed" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                    d="M13.875 18.825A10.05 10.05 0 0112 19c-4.478 0-8.268-2.943-9.543-7a9.97 9.97 0 011.563-3.029m5.858.908a3 3 0 114.243 4.243M9.878 9.878l4.242 4.242M9.88 9.88l-3.29-3.29m7.532 7.532l3.29 3.29M3 3l3.59 3.59m0 0A9.953 9.953 0 0112 5c4.478 0 8.268 2.943 9.543 7a10.025 10.025 0 01-4.132 5.411m0 0L21 21\">
+                </path>
+            </svg>
+        @endif
+    @else
+        <span class="text-sm">{{ $obfuscated }}</span>
     @endif
 </div>
--- src/Tables/Columns/ObfuscatedColumn.php
+++ src/Tables/Columns/ObfuscatedColumn.php
@@ -9,6 +9,11 @@ class ObfuscatedColumn extends TextColumn
     protected string $view = 'randomparanoia-filament-column-obfuscation::tables.columns.obfuscated-column';
 
     protected string $obfuscationPattern = 'default';
+    protected int $showFirstChars = 0;
+    protected int $showLastChars = 0;
+    protected string $obfuscationCharacter = '*';
+    protected bool $revealOnClick = true; // Default to true as your HTML already has click behavior
+    protected bool $revealOnHover = false;
 
     public function obfuscationPattern(string $pattern): static
     {
@@ -17,25 +22,90 @@ class ObfuscatedColumn extends TextColumn
         return $this;
     }
 
+    public function showFirstChars(int $count): static
+    {
+        $this->showFirstChars = $count;
+
+        return $this;
+    }
+
+    public function showLastChars(int $count): static
+    {
+        $this->showLastChars = $count;
+
+        return $this;
+    }
+
+    public function obfuscationCharacter(string $character): static
+    {
+        $this->obfuscationCharacter = $character;
+
+        return $this;
+    }
+
+    public function revealOnClick(bool $condition = true): static
+    {
+        $this->revealOnClick = $condition;
+        $this->revealOnHover = $condition ? false : $this->revealOnHover;
+
+        return $this;
+    }
+
+    public function revealOnHover(bool $condition = true): static
+    {
+        $this->revealOnHover = $condition;
+        $this->revealOnClick = $condition ? false : $this->revealOnClick;
+
+        return $this;
+    }
+
     public function getObfuscatedValue(?string $state): string
     {
-        if ($state === null) {
+        if ($state === null || $state === '') {
             return '';
         }
 
+        // If using custom first/last chars settings
+        if ($this->showFirstChars > 0 || $this->showLastChars > 0) {
+            $length = mb_strlen($state);
+            $firstChars = $this->showFirstChars > 0 ? mb_substr($state, 0, $this->showFirstChars) : '';
+            $lastChars = $this->showLastChars > 0 ? mb_substr($state, -$this->showLastChars) : '';
+
+            $middleLength = $length - $this->showFirstChars - $this->showLastChars;
+            $middleChars = $middleLength > 0 ? str_repeat($this->obfuscationCharacter, $middleLength) : '';
+
+            return $firstChars . $middleChars . $lastChars;
+        }
+
+        // Using predefined patterns
         return match ($this->obfuscationPattern) {
-            'name' => substr($state, 0, 1).str_repeat('*', strlen($state) - 1),
-            'phone' => substr($state, 0, 3).str_repeat('*', strlen($state) - 6).substr($state, -3),
+            'name' => mb_substr($state, 0, 1) . str_repeat($this->obfuscationCharacter, mb_strlen($state) - 1),
+            'phone' => mb_substr($state, 0, 3) . str_repeat($this->obfuscationCharacter, mb_strlen($state) - 6) . mb_substr($state, -3),
             'email' => (function (string $email) {
                 $parts = explode('@', $email);
                 $username = $parts[0];
                 $domain = $parts[1] ?? '';
 
-                return substr($username, 0, 2).str_repeat('*', strlen($username) - 2).'@'.$domain;
+                return mb_substr($username, 0, 2) . str_repeat($this->obfuscationCharacter, mb_strlen($username) - 2) . '@' . $domain;
             })($state),
-            'address' => substr($state, 0, 5).str_repeat('*', strlen($state) - 5),
-            'locality' => substr($state, 0, 2).str_repeat('*', strlen($state) - 2),
-            default => substr($state, 0, 1).str_repeat('*', strlen($state) - 1),
+            'address' => mb_substr($state, 0, 5) . str_repeat($this->obfuscationCharacter, mb_strlen($state) - 5),
+            'locality' => mb_substr($state, 0, 2) . str_repeat($this->obfuscationCharacter, mb_strlen($state) - 2),
+            default => str_repeat($this->obfuscationCharacter, mb_strlen($state)),
         };
+    }
+
+    public function getRevealOnClick(): bool
+    {
+        return $this->revealOnClick;
+    }
+
+    public function getRevealOnHover(): bool
+    {
+        return $this->revealOnHover;
+    }
+
+    public function getObfuscationCharacter(): string
+    {
+        return $this->obfuscationCharacter;
     }
 }
